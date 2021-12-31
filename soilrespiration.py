696f8f7ec9dab5cb88608b20f8d1ad05d27f7112
solomelittle <83061203+solomelittle@users.noreply.github.com>
2021-12-31 23:37:27 +0100

    Slopes calculated for each fluxid

--- soilrespiration.py
+++ soilrespiration.py
@@ -21,20 +21,16 @@ Rfielddata = pd.merge(Rfielddata, collar_dim, on="turfID")
 # Removing cut entries 
 Rfielddata = Rfielddata[Rfielddata.cut != 'cut']
 #Tair = Rfielddata.groupby(['fluxID']).mean()
-Rfielddata['datetime'] = pd.to_datetime(Rfielddata['datetime'])  
-groupedID = Rfielddata.groupby(['fluxID'])#.apply(['datetime']-['datetime'].min())
-
-#groupedID['time_delta'] = (groupedID['datetime'] - groupedID['datetime'].min())/ np.timedelta64(1,'s')
-
+groupedID = Rfielddata.groupby(['fluxID'])
 
  #%% Organizing Array by FluxID
- 
+#FluxIDs corresponding to type SoilR
 id_list = [27,42,47,72,112,121,158,176,201,210,231,256,292,305,308,339,363,385,419,439,466,477,548,561,580,
          587,632,635,666,669,693,702,739,756,793,796,827,830,874,897,908,923,962,985,1007,1012,1031,1053,
          1159,1162,1193,1196,1246,1274,1277,1294,1312,1317,1332,1333,1350,1374,1381]
 
-data = Rfielddata
-# create a new data array that has the number of entries = number of id numbers
+data = Rfielddata[["datetime", "CO2", "temp_air", "turfID", "campaign", "fluxID", "campaign", "area_m2", "volume_L"]]
+# create a new data array that has the number of entries = number of id numbers 
 new_data = np.zeros((len(id_list)), dtype = list)
 
 for i in range(len(id_list)):
@@ -50,79 +46,32 @@ for i in range(len(data)):
     for k in range(len(id_list)):
 
         # if the ID number of the current data point is equal to some k, add it to the k list
-        if current_point[16] == id_list[k]:
-
+        if current_point[5] == id_list[k]:
             new_data[k].append(current_point)
 
 # restructure the list of arrays into a bigger array
 for i in range(len(new_data)):
     new_data[i] = np.array(new_data[i])
-    new_data[i][:,1] = pd.to_datetime(new_data[i][:,1])
-# if I want to isolate the data with id number 100 or 200 and look at the concentrations then
-time_delta = np.array(len(id_list))
-new_data[:][:,1] = pd.to_datetime(new_data[:][:,1]
-                                  
-new_data [:]
-for i in range(len(id_list)):
-  time_delta[i,:]  = (new_data[i][:,1] - new_data[i][:,1].min())/ np.timedelta64(1,'s')
-  # try to make date ordinal thing work for you
-df = pd.DataFrame(list, columns=['date', 'value'])
-df.date =pd.to_datetime(df.date)
-df['date_ordinal'] = pd.to_datetime(df['date']).map(dt.datetime.toordinal)
-slope, intercept, r_value, p_value, std_err = stats.linregress(df['date_ordinal'], df['value'])
-#
-conc100s = new_data[0][:,1:2]
-conc200s = new_data[1][:,1:2]
-print(conc100s)
-print(conc200s)
 
 
+# %% Slope calculation
 
-# %%
+for i in range(len(new_data)):
+    dates = pd.to_datetime(new_data[i][:,0])
+    tempdates = (dates[:]-dates[:].min())/np.timedelta64(1,'s')
+    new_data[i][:,0] = tempdates
 
-# IDlist = [27,42,47,72,112,121,158,176,201,210,231,256,292,305,308,339,363,385,419,439,466,477,548,561,580,
-#          587,632,635,666,669,693,702,739,756,793,796,827,830,874,897,908,923,962,985,1007,1012,1031,1053,
-#          1159,1162,1193,1196,1246,1274,1277,1294,1312,1317,1332,1333,1350,1374,1381]
-# N = len(IDlist)
-# for i in len(Rfielddata):
-#     for k in len(IDlist):
-#         if Rfielddata['fluxID'][i] == IDlist[k]:
-#             Rfielddata['datetime','CO2','fluxID'][i]
-        
-#df_new = Tair.groupby.get_group['47']R
-#def date_delta(Rfielddata)
-# for Rfielddata['fluxID'] == 47:
-#     Rfielddata['time_delta'] = (Rfielddata['datetime'] - Rfielddata['datetime'].min())/ np.timedelta64(1,'s')
-       # return Rfielddata
-#Rfielddata.groupby('fluxID').pipe(lambda grp: grp.datetime / grp.size().sum())
-#Then, we need to change the date to integer
+slopelist=np.zeros(len(new_data))
+# 60 NEEDS FIXING
+for i in range(len(new_data)):
+    
+    slopelist[i] = np.polyfit(new_data[i][:,0].astype(float), new_data[i][:,1].astype(float),1)[0]
+
 #%%
-slopes = np.ones(64) # placeholder for when I figure out the time_delta for each fluxID
 
-#df['duration'] = df.groupby('user_id')['interval'].transform('sum')
- # def soil_respiration(m):
- #     atm_pressure = 1 # atmospheric pressurre, assumed 1 atm
- #     R = 0.082057 #gas constant, in L*atm*K^(-1)*mol^(-1)
- #     Rfielddata.iloc[:,4] = Rfielddata.iloc[:,4]+273.15 # converting to Kelvin for flux calc
- #     slope = ones(10)
- #     for i in range(10)
- #     slope(i) = linregress(Rfielddata.iloc[:,1,],Rfielddata.iloc[:,2])[1]
- #     flux = (slope * atm_pressure *(tube_volume + volume_L)/(R * temp_airavg * area_m2)
+def soil_respiration(m):
+    atm_pressure = 1 # atmospheric pressurre, assumed 1 atm
+    R = 0.082057 #gas constant, in L*atm*K^(-1)*mol^(-1)
+   # new_data[:,2] = new_data[:,2]+273.15 # converting to Kelvin for flux calc FIX THIS THIS IS POORLY INDEXED
+    flux = (slope * atm_pressure *(tube_volume + volume_L)/(R * temp_airavg * area_m2)
 
-
-# def model(Rfielddata, delta):
-#     y = Rfielddata[['CO2']].values
-#     X = Rfielddata[['date_delta']].values
-#     return np.squeeze(LinearRegression().fit(X, y).predict(delta))
-
-# def group_predictions(df, date):
-#     date = pd.to_datetime(date)
-#     df.date = pd.to_datetime(df.date)
-
-#     day = np.timedelta64(1, 's')
-#     mn = df.date.min()
-#     df['date_delta'] = df.date.sub(mn).div(day)
-
-#     dd = (date - mn) / day
-
-#     return df.groupby('group').apply(model, delta=dd)
